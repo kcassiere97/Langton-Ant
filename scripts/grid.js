1f236c302f1674208e8b503d7a1009c87a563a26
Scout2012 <jcbpowell12@gmail.com>
2019-09-11 01:32:34 -0700

    center square off slightly :( if someone wants to fix

--- scripts/grid.js
+++ scripts/grid.js
@@ -1,24 +1,23 @@
 var width = window.innerWidth;
 var height = window.innerHeight;
-var pixel_size = 10;
 
 var canvas = document.getElementById("canvas");
 var context = canvas.getContext("2d");
 
-canvas.width = width
-canvas.height = height
+canvas.width = width;
+canvas.height = height;
 
 function drawBoard(){
+    context.beginPath()
+
     for(var i = 0; i <= width; i += 40){
-        context.moveTo(0.5 + i + pixel_size, pixel_size)
-        context.lineTo(0.5 + i + pixel_size, height + pixel_size)
+        for(var j = 0; j <= height; j += 40){
+            context.rect(i, j, 40, 40);
+        }
     }
 
-    for(var i = 0; i <= height; i += 40){
-        context.moveTo(pixel_size, 0.5 + i + pixel_size)
-        context.lineTo( width + pixel_size, 0.5 + i + pixel_size)
-    }
     context.strokeStyle = "black";
+    context.fillRect((width/2), (height/2), 40, 40);
     context.stroke();
 }
 
