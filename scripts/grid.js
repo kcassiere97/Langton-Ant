ba53518b73310066259f3130834941e3c9cfde2f
Jacob Powell <jcbpowell12@gmail.com>
2019-09-15 18:45:55 -0700

    modified and improved the ant movement

--- scripts/grid.js
+++ scripts/grid.js
@@ -1,11 +1,14 @@
-var width = window.innerWidth;
-var height = window.innerHeight;
+let width = 1600;
+let height = 1600;
 
-var canvas = document.getElementById("canvas");
-var context = canvas.getContext("2d");
+let start_x = width/2;
+let start_y = height/2;
+
+let canvas = document.getElementById("canvas");
+let context = canvas.getContext("2d");
 
 canvas.width = width;
-canvas.height = height;
+canvas.height = height
 
 function drawBoard(){
     context.beginPath()
@@ -17,8 +20,23 @@ function drawBoard(){
     }
 
     context.strokeStyle = "black";
-    context.fillRect((width/2), (height/2), 40, 40);
+    context.fillRect(start_x, start_y, 40, 40);
     context.stroke();
 }
 
+function moveAnt(){
+       context.beginPath();
+	context.strokeStyle = "black"
+       context.fillStyle = "red";
+	context.fillRect(start_x, start_y, 40, 40)
+	context.fillStyle = "yellow";
+	context.strokeStyle = "black"
+       context.fillRect(start_x += 40, start_y, 40, 40);
+       context.stroke();
+}
+
 drawBoard();
+setInterval(moveAnt, 3000);
+//moveAnt();
+//setInterval(function(){
+//alert("hello"); }, 3000};
